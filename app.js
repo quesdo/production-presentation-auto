9556ba73f5b80098f266b779f5f5769df29c063c
WEYDERT Paul <PWT2@dsone.3ds.com>
2026-06-16 10:07:30 +0200

    Show General and hide virtual_training on Finish

--- app.js
+++ app.js
@@ -497,6 +497,10 @@ function showEndScreen() {
     const slideText = textContent.querySelector('.slide-text');
     const nextBtn = document.getElementById('nextBtn');
 
+    // Show General and hide virtual_training when finishing
+    toggleVisibility("General", true);
+    toggleVisibility("virtual_training", false);
+
     // Animate out
     textContent.classList.remove('show');
     nextBtn.classList.remove('show');
